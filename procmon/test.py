beec0a2ba8ff0017bc4eac435093473f46d23a74
Siu Kwan Lam <michael.lam.sk@gmail.com>
2018-05-08 10:02:42 -0500

    Make procmon use process to avoid the GIL holding up

---
+++ procmon/test.py
@@ -0,0 +1,11 @@
+import time
+from procmon import record, CpuMon
+
+def main():
+    with record([CpuMon]) as r:
+        print("HAHAHA")
+        time.sleep(0.5)
+    print(r.get_data())
+
+if __name__ == '__main__':
+    main()
